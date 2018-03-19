d4416e084c49151ad872d260bad2dcb193ff3991
Petya Djanovska <petya.djanovska@gmail.com>
2018-03-19 17:30:13 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,11 +7,13 @@ class Student
   attr_reader :id
 
   def initialize (name, grade, id = nil)
+    @id = id
     @name = name
     @grade = grade
   end
 
   def self.create_table
+    sql = SQL CREATE TABLE IF NOT EXISTS students(id INTEGER PRIMARY KEY, name TEXT, grade INTEGER)
     DB[:conn]
   end
 
